56881121c8b2c9a1b8e86db66e7cb3cf3bbec7e1
cohort20 <sophia.jiyu@gmail.com>
2023-05-17 18:49:34 -0700

    add 2-seletion-sort, 2-O

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+  *selection_sort-sorts an array of integers in ascending order
+  *@array: input array
+  *@size: size of the array
+  *Return: void
+  */
+void selection_sort(int *array, size_t size)
+{
+	size_t index, next;
+	size_t position;
+	int min;
+
+	if (array == NULL || size < 2)
+		return;
+	index = 0;
+	while (index < (size - 1))
+	{
+		position = index;
+		next = index + 1;
+		while (next < size)
+		{
+			if (array[position] > array[next])
+			{
+				position = next;
+			}
+			next = next + 1;
+		}
+		if (position != index)
+		{
+			min = array[index];
+			array[index] = array[position];
+			array[position] = min;
+		}
+		print_array(array, size);
+		index = index + 1;
+	}
+}
